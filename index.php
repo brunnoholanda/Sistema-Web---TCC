05a105fa983ac676d6fb87d0b254a7f927f0e606
Brunno <brunnoholanda@hotmail.com>
2017-09-15 21:27:29 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title>Sistema de Patrimônio de Órgãos Públicos e Privados - Login</title> 
+   </head>
+   <body>
+
+ 
+<div class="container login" align="center">
+ 
+<h3>Acesso ao Sistema</h3><p>
+             
+ 
+<form action="" method="POST">
+               <label for="email">E-mail:</label>
+                
+               <input type="text" name="email" id="email" required/>
+                
+               <p>
+               <label for="senha">Senha:</label>
+                
+               <input type="password" name="senha" id="senha" required/>
+                
+                <p>
+               <input type="submit" value="Enviar" name="Enviar"/>
+            </form>
+ 
+         </div>
+ 
+      </div>
+ 
+   </body>
+</html>
