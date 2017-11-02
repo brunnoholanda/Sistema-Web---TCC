4c0c97996e854b5214d9d6426af3e8e06211fc76
Brunno <brunnoholanda@hotmail.com>
2017-11-01 23:17:21 -0200

    novos scripts

---
+++ admin.php
@@ -0,0 +1,17 @@
+<?php
+	/* session_start();
+		if (!(isset($_SESSION['login'])))
+		{
+			header("location: admin.php");
+		}; */
+?>
+
+<html>
+	<head>
+		<meta charset="UTF=8">
+		<title></title>
+	</head>
+	<body>
+	Bem vindo!
+	</body>
+</html>
