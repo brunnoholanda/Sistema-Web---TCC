13a2c65a9983e0054fd2b0fec4a5df57fbc558e3
Brunno <brunnoholanda@hotmail.com>
2017-11-01 23:16:19 -0200

    Novos scripts

--- index.php
+++ index.php
@@ -1,34 +1,55 @@
-<!DOCTYPE HTML>
 <html>
-   <head>
-      <meta charset="UTF-8">
-      <title>Sistema de Patrimônio de Órgãos Públicos e Privados</title> 
-   </head>
-   <body>
-
- 
-<div class="container login" align="center">
- 
-<h3>Acesso ao Sistema</h3><p>
-             
- 
-<form action="" method="POST">
-               <label for="email">E-mail:</label>
-                
-               <input type="text" name="email" id="email" required/>
-                
-               <p>
-               <label for="senha">Senha:</label>
-                
-               <input type="password" name="senha" id="senha" required/>
-                
-                <p>
-               <input type="submit" value="Enviar" name="Enviar"/>
-            </form>
- 
-         </div>
- 
-      </div>
- 
-   </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Início</title>
+	</head>
+	<body>
+		<form method="POST" action="index.php">
+			Usuário
+			<input type="text" name="login"><br>
+			Senha
+			<input type="password" name="senha"><br>
+			
+			<input type="submit" name="Entrar" value="Entrar">
+			
+			
+		<?php
+			if ($_POST) 
+			{
+				include 'C:\Program Files\EasyPHP-DevServer-14.1VC9\data\localweb\projects\Sistema\classes\DAO\Conexao.php';
+				include 'C:\Program Files\EasyPHP-DevServer-14.1VC9\data\localweb\projects\Sistema\classes\DAO\UsuarioDAO.php';
+				
+				$usuarioDAO = new UsuarioDao();
+				
+				$login =  $_POST['login'];
+				$senha =  $_POST['senha'];
+				
+				$user = $usuarioDAO->login($senha, $login);
+				
+				if ($user == true)
+				{
+					$_SESSION['login']=$login;
+					$_SESSION['senha']=$senha;
+				header("location: admin.php");
+				}
+				else
+				{
+					header ("location: index.php?erro=senha");
+				}
+			}
+		?>
+		
+		<?php
+			if ($_GET)
+			{
+				if (isset($_GET['erro']))
+				{
+					echo "Usuário ou Senha inválidos!";
+				}
+			}
+		
+		
+		
+		?>
+	</body>
 </html>
